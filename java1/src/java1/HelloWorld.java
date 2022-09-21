19cf8296c9f48bf2f5313892311ed67f9286609f
Javier Prz <58307741+jvprz@users.noreply.github.com>
2022-09-21 20:58:25 +0200

    Add files via upload

---
+++ java1/src/java1/HelloWorld.java
@@ -0,0 +1,19 @@
+/* 
+ * @author $[user]
+ *
+ * ${tags} ${date}
+ */
+package java1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//Call print hello world method
+		start();
+	}
+	public static void start() {
+		//Declare hello world message
+		String hw = "Hello World!";
+		//Pint message
+		System.out.println(hw);
+	}
+}
